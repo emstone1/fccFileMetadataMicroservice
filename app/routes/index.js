8d4b92d638569b27d5ca180085f074b464f0a179
emstone <ethan.marc.stone@gmail.com>
2017-01-09 20:51:54 +0000

    project complete

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,8 +1,15 @@
 'use strict';
+var multer = require('multer');
+var     autoReap = require('multer-autoreap');
 
 module.exports = function (app) {
     app.route('/')
         .get(function (req, res) {
             res.sendFile(process.cwd() + '/public/index.html');
         });
+    app.use(autoReap);
+    app.post('/', multer({dest:process.cwd()+'/temp/'})
+        .single('file'), function(req, res){
+    res.send(JSON.stringify({"fileSize": req.file.size}))
+    });
 };
--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 'use strict';
 const express = require('express'),
     port = process.env.PORT || 8080,
-    routes = require('./app/routes/index.js')
-
+    routes = require('./app/routes/index.js'),
+    autoReap = require('multer-autoreap')
 var app = express();
 
 routes(app);
-
+app.use(autoReap);
 app.listen(port, function () {
     console.log('Listening on port '+ port);
 });
